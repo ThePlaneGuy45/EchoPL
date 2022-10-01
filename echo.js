5decf785e820f1045748ce06479bcf8e7793fef8
ThePlaneGuy45 <69761838+ThePlaneGuy45@users.noreply.github.com>
2022-09-30 22:27:28 -0400

    Create echo.js

---
+++ echo.js
@@ -0,0 +1,35 @@
+// Main Functions, (plugin loading, file fetching, etc)
+const script = {};
+script.load = function(file) {
+  import(file).then(value => {
+    return(value);
+  }).catch(err => {
+    return(err);
+  });
+}
+
+const plunginList = {};
+const pl = {};
+pl.load = function(file) {
+  import(file).then(value => {
+    if(pluginList.hasOwnProperty(value.id)) {
+      console.error("Unable To Duplicate Plugin IDs");
+    } else {
+      pluginList.[value.id] = value.functions;
+    }
+  }).catch(err => {
+    console.error(err);
+  });
+}
+
+const echo = {
+  "ver": "0.0.1",
+  "version": "0.0.1",
+  "v": "0.0.1",
+  "info": "Echo Plugin Loader v0.0.1",
+  "information": "Echo Plugin Loader v0.0.1",
+  "script": script(),
+  "s": script(),
+  "plugin": pl(),
+  "pl": pl()
+};
